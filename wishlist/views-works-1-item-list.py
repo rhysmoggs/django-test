ceb97146723b92befe8aca04336ba8edfb1364fc
rhysmoggs <rhysmoggs@hotmail.com>
2022-11-15 12:46:07 +0000

    wishlist update

--- wishlist/views-works-1-item-list.py
+++ wishlist/views-works-1-item-list.py
@@ -22,55 +22,29 @@ def get_wishlist(request):
     return render(request, template, context)
 
 
-# def add_to_wishlist(request, product_id):
-#     """Adds product to user wishlist"""
-
-#     product = get_object_or_404(Product, pk=product_id)
-#     print(product)
-
-#     user = get_object_or_404(UserProfile, user=request.user)
-#     print(user)
-
-#     obj, created = Wishlist.objects.get_or_create(
-#         product=product,
-#         user=user
-#     )
-#     print(obj, created)
-#     print(obj)
-#     print(created)
-
-#     redirect_url = request.POST.get('redirect_url')
-
-#     return redirect(reverse('product_detail', args=[product.id]))
-
-
 def add_to_wishlist(request, product_id):
     """Adds product to user wishlist"""
 
     product = get_object_or_404(Product, pk=product_id)
-    print(product)
-
     user = get_object_or_404(UserProfile, user=request.user)
-    print(user)
-
-    #rename obj to wishlist. check if necessary to use created?
-    #works well, just need to figure out a way to add all to one list. create wishlist list here
-    #or in Wishlist model.py?
-    obj, created = Wishlist.objects.get_or_create(
-        product=product,
-        user=user
-    )
-    print(obj, created)
-    print(obj)
-    print(created)
-
-    if created is True:
-        messages.success(request, f'{product.name} added to your wishlist.')
-    else:
-        messages.info(request, f'{product.name} already in your wishlist.')
 
     redirect_url = request.POST.get('redirect_url')
 
+    if Wishlist.objects.filter(product=product, user=user).exists():
+        print(Wishlist.objects.filter(product=product, user=user).exists())
+        messages.info(request, f'{product.name} already in your wishlist.')
+    # elif wishlist:
+    #     Wishlist.objects.create(
+    #         user=user,
+    #         product=product
+    #     )
+    else:
+        wishlist, created = Wishlist.objects.get_or_create(user=user, product=product)
+        # wishlist = Wishlist.objects.filter(user=user)
+        wishlist.save()
+        messages.success(request, f'{product.name} added to your wishlist.')
+
+    # return redirect(redirect_url)
     return redirect(reverse('product_detail', args=[product.id]))
 
 
